f72ccf59c20c2e8aed1a511d8d366a64a55a354f
Matheus <130110444+MatheusAlexandreFL@users.noreply.github.com>
2026-03-12 16:11:33 -0300

    Add files via upload

---
+++ BuscaCEP.c
@@ -0,0 +1,72 @@
+    #include <stdio.h>
+#include <string.h>
+
+typedef struct _Endereco Endereco;
+
+struct _Endereco
+{
+	char logradouro[72];
+	char bairro[72];
+	char cidade[72];  
+	char uf[72];
+	char sigla[2];
+	char cep[8];
+	char lixo[2]; // Ao Espaço no final da linha + quebra de linha
+};
+
+int main(int argc, char**argv)
+{
+	FILE *f;
+	Endereco e;
+	int qt;
+	int c;
+
+	if(argc != 2)
+	{
+		fprintf(stderr, "USO: %s [CEP]", argv[0]);
+		return 1;
+	}
+
+	c = 0;
+	printf("Tamanho da Estrutura: %ld\n\n", sizeof(Endereco));
+	f = fopen("cep_ordenado.dat","rb");
+	if (!f) {
+        perror("Erro ao abrir arquivo");
+        return 1;
+    }
+	fseek(f, 0, SEEK_END);
+	long tamanhoBytes = ftell(f);
+    long totalRegistros = tamanhoBytes / sizeof(Endereco);
+	qt = fread(&e,sizeof(Endereco),1,f);
+	int inicio = 0;
+    int fim = totalRegistros - 1;
+
+    while (inicio <= fim) {
+        c++;
+        int meio = (inicio + fim) / 2;
+
+        
+        fseek(f, meio * sizeof(Endereco), SEEK_SET);
+        fread(&e, sizeof(Endereco), 1, f);
+		// argv[1] < e.cep  => strcmp(argv[1],e.cep) < 0
+		// argv[1] > e.cep  => strcmp(argv[1],e.cep) > 0
+		// argv[1] == e.cep  => strcmp(argv[1],e.cep) == 0
+		int comparacao = strncmp(argv[1], e.cep, 8);
+		if (comparacao == 0) {
+            printf("Encontrado em %d tentativas!\n", c);
+            printf("%.72s\n%.72s\n%.72s\n%.72s\n%.2s\n%.8s\n", 
+                    e.logradouro, e.bairro, e.cidade, e.uf, e.sigla, e.cep);
+            fclose(f);
+            return 0;
+        } else if (comparacao < 0) {
+            fim = meio - 1; // O alvo está na metade esquerda
+        } else {
+            inicio = meio + 1; // O alvo está na metade direita
+        }
+    }
+
+    printf("CEP não encontrado. Total de leituras: %d\n", c);
+    fclose(f);
+    return 0;
+}
+
